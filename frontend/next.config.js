acb4dcb5948dca1f118164a8dbae981c1344fe91
xinhuawu0501 <b06106035@ntu.edu.tw>
2023-04-04 12:38:01 +0800

    feat(frontend): rewrite backend api endpoint to enable api call in docker network

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: `${process.env.API_ENDPOINT}/:path*`,
+        destination: `${process.env.BACKEND_HTTP_ADDRESS}/:path*`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

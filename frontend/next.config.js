0c843b8afea6629113c3ca67aac9152c9e8b5b42
xinhuawu0501 <b06106035@ntu.edu.tw>
2023-04-04 13:53:40 +0800

    refactor(frontend): get backend api route from next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const publicRuntimeConfig = {
+  BACKEND_API: process.env.API_ENDPOINT,
+};
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  publicRuntimeConfig,
   async rewrites() {
     return [
       {
